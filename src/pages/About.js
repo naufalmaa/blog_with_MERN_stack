96806a1196f453ff73390af6ae774474b382fc84
naufalmaviz <naufalmadzkia@gmail.com>
2023-09-27 17:03:13 +0700

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+                About Me
+            </h1>
+            <p className='mx-auto loading-relaxed text-base mb-4'>
+            Hello! My Name is Naufal Muhamad Adzkia. I'm 25. I'm just an inquisitive lad who dance with the data.
+            </p>
+            <p className='mx-auto loading-relaxed text-base mb-4'>
+            Despite my background as a bachelor of geological engineering, I'm keen to learn more about data science. Yes, geologist and data scientist, what a wonderful pair of skillsets, huh? 
+            </p>
+            <p className='mx-auto loading-relaxed text-base mb-4'>
+            Guess now what? I'm learning about MERN Stack too! Now, I'm kinda web developer too!
+            </p>
+        </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='mb-20'>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+                Hello, Welcome to My Very First Blog with MERN Stack!
+            </h1>
+            <p className='mx-auto loading-relaxed text-base mb-4'>
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            </p>
+            <p className='mx-auto loading-relaxed text-base mb-4'>
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            </p>
+            <p className='mx-auto loading-relaxed text-base mb-4'>
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            </p>
+            <p className='mx-auto loading-relaxed text-base mb-4'>
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            </p>
+            <p className='mx-auto loading-relaxed text-base mb-4'>
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            the quick brown fox jumped over the lazy dog.
+            </p>
+        </div>
+    );
+}
+
+export default Home
